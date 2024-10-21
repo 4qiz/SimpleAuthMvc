b402d42485eea5165b5ea2478cc60b55e5c3c077
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2024-10-21 19:57:48 +0300

    fix

--- ScaffoldedIdentityMvc/Models/AppUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace ScaffoldedIdentityMvc.Models
-{
-    public class AppUser : IdentityUser
-    {
-        public string MyProperty { get; set; } = string.Empty;
-    }
-}
--- ScaffoldedIdentityMvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ScaffoldedIdentityMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
